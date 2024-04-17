a467641904502f539478f5c45f7a08b31a37dd21
satopon1005 <144639995+satopon1005@users.noreply.github.com>
2024-04-17 15:21:53 +0900

    mapchip

---
+++ src/MapChip/MapChip.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "../Common.h"
+
+constexpr int MAPCHIP_NUM_X = 16;
+constexpr int MAPCHIP_NUM_Y = 9;
+
+constexpr int MAPCHIP_SIZE_X = SCREEN_SIZE_X / 16;
+constexpr int MAPCHIP_SIZE_Y = SCREEN_SIZE_Y / 9;
+
+class MapChip
+{
+private:
+
+
+public:
+
+
+};
