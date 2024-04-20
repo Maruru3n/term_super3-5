d5b3c5d6233f41b86b01097b06aee56743b98c04
Maruru3n <fukushimaks121@icloud.com>
2024-04-20 22:32:00 +0900

    タイトルをすこーしいじった

--- src/Scene/SceneTitle/SceneTitle.cpp
+++ src/Scene/SceneTitle/SceneTitle.cpp
@@ -6,15 +6,34 @@
 void SceneTitle::Init()
 {
 	m_bg_handle = LoadGraph(TITLE_BG_HANDLE_PATH);
+	ui_handle[0] = LoadGraph("Data/Image/Title/titleName.png");
+	ui_handle[1] = LoadGraph("Data/Image/Title/titleUI.png");
+	ui_size = 1.0;
+	size_flag = false;
 }
 void SceneTitle::Step()
 {
+	//UIÇÃÉTÉCÉYÇïœÇ¶ÇÈèàóù
+	if (!size_flag) {
+		ui_size += 0.005;
+	}
+	else {
+		ui_size -= 0.005;
+	}
+	if (ui_size < 1.0) {
+		size_flag = false;
+	}
+	if (ui_size >1.2) {
+		size_flag = true;
+	}
 	if (Input::IsKeyPush(KEY_INPUT_RETURN))
 		g_current_scene_ID = Play;
 }
 void SceneTitle::Draw()
 {
-	DrawGraph(0, 0, m_bg_handle, true);
+	DrawRotaGraph(640, 360, 2,0.0,m_bg_handle, true);
+	DrawRotaGraph(640, 360, 1.0, 0.0, ui_handle[0], true);
+	DrawRotaGraph(640, 360, ui_size, 0.0, ui_handle[1], true);
 }
 void SceneTitle::Fin()
 {
--- src/Scene/SceneTitle/SceneTitle.h
+++ src/Scene/SceneTitle/SceneTitle.h
@@ -1,12 +1,15 @@
 #pragma once
 #include "../SceneBace/SceneBace.h"
 
-constexpr char TITLE_BG_HANDLE_PATH[] = { "Data/Image/Title/bg_title.png" };
+constexpr char TITLE_BG_HANDLE_PATH[] = { "Data/Image/Title/title.png" };
+
 
 class SceneTitle :public SceneBace
 {
 private:
-
+	int ui_handle[2];
+	float ui_size;
+	bool size_flag;
 public:
 	void Init();
 	void Step();
